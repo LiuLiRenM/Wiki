3e5c480907a523af8411aa78fac8793fd10c59fc
刘耀文 <lyw001@nit.edu.cn>
2021-08-10 11:51:08 +0800

    feat: 初始化wiki项目

---
+++ src/main/java/com/lyw/wiki/WikiApplication.java
@@ -0,0 +1,16 @@
+package com.lyw.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiuYaoWen
+ */
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
